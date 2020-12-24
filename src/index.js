b12fcc3a2dca034a2208aea8a4e1507f9732c7ad
itswagi <43064115+itswagi@users.noreply.github.com>
2020-12-24 23:24:54 +0500

    Moved typedefs to seperate file

--- src/index.js
+++ src/index.js
@@ -1,44 +1,7 @@
 const { PrismaClient } = require ('@prisma/client')
-const { ApolloServer, gql } = require('apollo-server')
-
-const typeDefs = gql`
-    type Books {
-        isbn: Int!
-        title: String
-        price: Float
-        quantity: Int
-        publisher_id: Int
-        publisher: Publishers!
-    }
-    type Publishers {
-        id: Int!
-        name: String
-        year_publication: Int
-    }
-    type Readers {
-        id: Int!
-        name: String
-        email: String
-        password: String
-        address: String
-        phone: String
-    }
-    type CheckedOut {
-        id: Int!
-        isbn_book: Int
-        book: [Books!]!
-        reader_id: Int
-        reader: [Readers!]!
-        checkedout_date: String
-        returned: Boolean
-        returned_date: String
-        duration: Int
-    }
-    type Query {
-        book: [Books]
-    }
-
-`
+const { ApolloServer } = require('apollo-server')
+const fs = require('fs');
+const path = require('path');
 
 const resolvers = {
     Query: {
@@ -48,23 +11,20 @@ const resolvers = {
     },
     Books: {
         publisher: async (parent, args, context) => {
-            console.log(parent.publisher_id)
             return context.prisma.publishers.findUnique({
                 where: {id : parent.publisher_id}
             }) 
         }
     }
-    // Publishers: async (parent, args, context) => {
-    //     console.log("1")
-    //     return context.prisma.publishers.findUnique({
-    //         where: {id : parent.publisher_id}
-    //     })
-    // }
 }
+
 const prisma = new PrismaClient()
 
 const server = new ApolloServer({
-    typeDefs,
+    typeDefs: fs.readFileSync(
+        path.join(__dirname, 'schema.graphql'),
+        'utf8'
+    ),
     resolvers,
     context: {
         prisma,
