b30a2b8a9ef83826e3b2793e5811c6befabbc7c3
itswagi <43064115+itswagi@users.noreply.github.com>
2020-12-31 14:20:26 +0500

    Added booksSlice

---
+++ library-frontend/src/features/books/booksSlice.js
@@ -0,0 +1,6 @@
+import { createStore } from '@reduxjs/toolkit'
+
+const initialState = [
+    {isbn: 1, title: 'First Book', price:  1.0, quantity: 1, publisher_id: 1},
+    {isbn: 2, title: 'Second Book', price:  1.0, quantity: 1, publisher_id: 1}
+]
