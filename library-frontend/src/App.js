7a8644f9358e7b0d63097a8b19c70e522dd9971f
itswagi <43064115+itswagi@users.noreply.github.com>
2021-01-02 15:48:42 +0500

    Added react-router-dom

--- library-frontend/src/App.js
+++ library-frontend/src/App.js
@@ -1,23 +1,40 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
 import './App.css';
 
 import { BooksList } from './features/books/BooksList'
 import { PublishersList } from './features/publishers/PublishersList'
 import { CheckedOutList } from './features/checkedouts/CheckedOutList'
 import { NavBar } from './features/NavBar/NavBar'
-
+import { Search } from './features/search/search'
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <div className="App">
         <NavBar />
         <div className="content">
-          <BooksList />
-          <PublishersList />
-          <CheckedOutList />
+          <Switch>
+            <Route exact path="/" render={ () => (
+                <>
+                  <BooksList />
+                  <PublishersList />
+                  <CheckedOutList />
+                </>
+              )}
+            />
+            <Route exact path="/search" component={Search}/>
+            <Redirect to="/" />
+          </Switch>
         </div>
-        
-    </div>
+      </div>
+    </Router>
+    
   );
 }
 
--- library-frontend/src/features/NavBar/NavBar.js
+++ library-frontend/src/features/NavBar/NavBar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import { FiSearch } from 'react-icons/fi'
 import { FaHome } from 'react-icons/fa'
 import { AiOutlineLogin, AiOutlineLogout} from 'react-icons/ai'
@@ -24,10 +25,15 @@ export const NavBar = () => {
     return(
         <nav>
             <div title="Home">
-                <FaHome size="1.5rem" color="white" stroke-width="3" />
+                <NavLink to="/">
+                    <FaHome size="1.5rem" color="white" stroke-width="3" />
+                </NavLink>
             </div>
             <div title="Search">
-                <FiSearch size="1.5rem" color="white" stroke-width="3" />
+                <NavLink to="/search">
+                    <FiSearch size="1.5rem" color="white" stroke-width="3" />
+                </NavLink>
+                
             </div>
             {renderLoginLogout()}
         </nav>
