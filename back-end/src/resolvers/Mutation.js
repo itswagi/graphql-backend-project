84a347823eb36d7b25f32cc873f960b6e4521727
itswagi <43064115+itswagi@users.noreply.github.com>
2021-01-03 15:31:30 +0500

    Minor fixes

--- back-end/src/resolvers/Mutation.js
+++ back-end/src/resolvers/Mutation.js
@@ -7,11 +7,8 @@ const { APP_SECRET, getUserId } = require('../utils')
 async function signup(parent, args, context, info) {
     try{
         const password = await bcrypt.hash(args.password, 10)
-  
         const user = await context.prisma.readers.create({ data: { ...args, password } })
-    
         const token = jwt.sign({ userId: user.id }, APP_SECRET)
-    
         return {
         token,
         user,
