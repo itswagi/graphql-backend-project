fb978f9cf2170bb55093a0835426d2bd1a07f57d
itswagi <43064115+itswagi@users.noreply.github.com>
2021-01-03 15:13:50 +0500

    Added Login Form

--- library-frontend/src/features/Login/Login.js
+++ library-frontend/src/features/Login/Login.js
@@ -12,12 +12,33 @@ const login = gql`
 
 export const Login = () => {
     const [addTodo] = useMutation(login)
+    // onClick={ () => {
+    //     //addTodo().then(result => console.log(result))
+    // }}
+    // let email
+    // let password
+    // const handleEmailChange = (event) => {
+    //     email = event.target.value
+    // }
+    // const handlePasswordChange = (event) => {
+    //     password = event.target.value
+    //     console.log(password)
+    // }
+    const handleLogin = () => {
+        const email = document.getElementById('email').value
+        const password = document.getElementById('password').value
 
+    }
     return (
-        <div className="item-list" onClick={ () => {
-            //addTodo().then(result => console.log(result))
-        }}>
-            Login
+        <div className="login">
+            <div className="upper">
+                <h1>Login</h1>
+            </div>
+            <div className="lower">
+                <input type="email" id="email" placeholder="Email" />
+                <input type="password" id="password" placeholder="Password"/>
+                <button onClick={handleLogin}>Login</button>
+            </div>
         </div>
     )
 }
