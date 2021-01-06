3089ff3e4f46faa8a8356f7451be38e7ee696bb5
itswagi <43064115+itswagi@users.noreply.github.com>
2021-01-06 20:00:10 +0500

    Added tailwind css

--- library-frontend/src/features/Login/SignupDiv.js
+++ library-frontend/src/features/Login/SignupDiv.js
@@ -4,6 +4,7 @@ import { authSignUp } from './loginSlice'
 
 export const SignupDiv = () => {
     const [showForm, setShowForm] = useState(false)
+
     const dispatch = useDispatch()
     const name = useRef()
     const email = useRef()
